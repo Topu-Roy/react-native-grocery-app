d96a21f27fcad2b87360c29bc6a0dee3c92da133
Topu Roy <topuroy143333@gmail.com.com>
2023-06-17 19:15:47 +0600

    done with this project

--- app/AppNavigator.js
+++ app/AppNavigator.js
@@ -2,8 +2,6 @@ import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Main from "./screens/Main";
 import ProductDetails from "./screens/tabs/ProductDetails";
-import Favorites from "./screens/tabs/Favorites";
-import Search from "./screens/tabs/Search";
 
 const Stack = createNativeStackNavigator();
 
